5b0114e76074104f684019272c2e5f597f0f43a0
Mohamed Fawzy <47891524+Mohamadfawzy@users.noreply.github.com>
2021-09-24 18:08:09 +0200

    setup

---
+++ CustomSearchBar/CustomSearchBar/ViewModels/MainPageVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomSearchBar.ViewModels
+{
+    public class MainPageVM
+    {
+        public MainPageVM()
+        {
+
+        }
+    }
+}
